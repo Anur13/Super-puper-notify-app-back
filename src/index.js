39aa7799158bef75907877fc16018862c8d3b1e7
Anur13 <akrupskyi13@gmail.com>
2021-11-13 16:30:00 +0200

    Created working structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
+// eslint-disable-next-line prefer-const
+// server = app.listen(config.port, () => {
+//   logger.info(`Listening to port ${config.port}`);
+// });
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
   server = app.listen(config.port, () => {
