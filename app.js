4a3560df459bd44ddb64aea6e7dd14b6df8b20bb
Anur13 <akrupskyi13@gmail.com>
2021-11-20 09:59:06 +0200

    Improved Structure

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const logger = require('morgan');
+
 
 const app = express();
+
+const loggerType = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(logger(loggerType))
 
 require('dotenv').config();
 
--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,21 @@
 const app = require("../app");
-
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
+const dbURL = process.env.MONGODB_URL;
+
+const connection = mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+connection.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running. Use our API on port: ${PORT}`);
+    })
+}).catch(e => {
+    console.log(e);
+    process.exit(1);
 })
+
