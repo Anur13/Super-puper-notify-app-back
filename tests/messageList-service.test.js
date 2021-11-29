c078ecded0b992e41a949531dbe0379394ee9f81
Anur13 <akrupskyi13@gmail.com>
2021-11-29 19:00:11 +0200

    WIP

---
+++ tests/messageList-service.test.js
@@ -0,0 +1,112 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const dbURL = process.env.MONGODB_URL;
+const MessageListService = require("../services/messageList-service");
+
+const MessageList = require("../models/messageList-model");
+describe("create", () => {
+  let connection;
+
+  beforeAll(async () => {
+    connection = await mongoose.connect(dbURL, {
+      useNewUrlParser: true,
+    });
+  });
+
+  afterAll(async () => {
+    await connection.close();
+  });
+
+  beforeEach(() => {});
+
+  afterEach(() => {});
+
+  it("should expand the object with new fields and save it", async () => {
+    const mockList = { title: "some-user-id" };
+    await MessageListService.create(mockList);
+
+    const insertedList = await MessageList.findOne({ title: "some-user-id" });
+
+    expect(insertedList).toEqual(
+      expect.objectContaining({
+        folderId: null,
+        sys: expect.objectContaining({
+          created: expect.any(Date),
+          lastUpdated: expect.any(Date),
+        }),
+        messagesId: [],
+        title: "some-user-id",
+      }),
+    );
+    await MessageList.findOneAndDelete({ title: "some-user-id" });
+  });
+
+  it("should return null when new list created and if list with the same folderId and title exists", async () => {
+    const mockList = { title: "test", folderId: "1" };
+
+    await MessageListService.create(mockList);
+    const result = await MessageListService.create(mockList);
+    expect(result).toEqual(null);
+    await MessageList.findOneAndDelete({ title: "test" });
+  });
+
+  it("should return null when new list created and if list with folderId = null and same title exists", async () => {
+    const mockList = { title: "test", folderId: null };
+
+    await MessageListService.create(mockList);
+    const result = await MessageListService.create(mockList);
+    expect(result).toEqual(null);
+    await MessageList.findOneAndDelete({ title: "test" });
+  });
+
+  it(
+    "should return list when new list created, folderId = null," +
+      " and there exists a list with folderId and same title",
+    async () => {
+      const mockListNoFolderId = { title: "No folder id", folderId: null };
+      const mockListWithFolderId = { title: "With folder id", folderId: 1 };
+
+      await MessageListService.create(mockListWithFolderId);
+      const result = await MessageListService.create(mockListNoFolderId);
+
+      expect(result).toEqual(
+        expect.objectContaining({
+          folderId: null,
+          sys: expect.objectContaining({
+            created: expect.any(Date),
+            lastUpdated: expect.any(Date),
+          }),
+          messagesId: [],
+          title: "No folder id",
+        }),
+      );
+      await MessageList.findOneAndDelete({ title: "No folder id" });
+      await MessageList.findOneAndDelete({ title: "With folder id" });
+    },
+  );
+  it("on update should return object with updated fields and lastUpdated field", async () => {
+    const newList = await MessageListService.create({ title: "New list" });
+
+    // const df = newList.to;
+    const test = { ...newList, title: "Updated list", messagesId: [3, 4] };
+    console.log(newList, test);
+    // const updatedList = MessageListService.update({
+    //   ...newList,
+    //   title: "Updated list",
+    //   messagesId: [3, 4],
+    // });
+
+    // expect(updatedList).toEqual(
+    //   expect.objectContaining({
+    //     folderId: null,
+    //     sys: expect.objectContaining({
+    //       created: expect.any(Date),
+    //       lastUpdated: expect.any(Date),
+    //     }),
+    //     messagesId: [3, 4],
+    //     title: "Updated list",
+    //   }),
+    // );
+    // expect(updatedList.sys.lastUpdated).not.toEqual(newList.sys.lastUpdated);
+  });
+});
