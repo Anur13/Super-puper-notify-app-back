ef0d5bc1ca8967ddb7ede2f75b956651743b9872
Anur13 <akrupskyi13@gmail.com>
2022-01-31 17:12:42 +0200

    deleted junk code

--- middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-
-const authMiddleware = function(req,res,next){
-  console.log(req.headers)
-  next()
- }
-
- module.exports = authMiddleware;
--- routers/messageList-router.js
+++ routers/messageList-router.js
@@ -2,10 +2,9 @@ const express = require("express");
 const Constants = require("../common/constants/constants");
 const router = express.Router();
 const MessageListController = require("../controller/messageList-controller");
-const authMiddleware = require("../middlewares/auth");
 
 router.post(Constants.routes.messageList.create, MessageListController.create);
-router.get(Constants.routes.messageList.get, authMiddleware, MessageListController.get);
+router.get(Constants.routes.messageList.get, MessageListController.get);
 router.delete(Constants.routes.messageList.delete, MessageListController.delete);
 router.patch(Constants.routes.messageList.update, MessageListController.update);
 
